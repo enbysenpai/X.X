e7e7b0dcdad2e064d1971bb4aa452b59d6be0fc0
Antoniu1001 <ddumitru128@gmail.com>
2025-06-02 02:16:00 +0300

    Resources

--- VA-front/pages/index.js
+++ VA-front/pages/index.js
@@ -1,10 +1,10 @@
-import { useState, useRef, useEffect, useLayoutEffect } from 'react'
+import React, { useState, useRef, useEffect, useLayoutEffect } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import ReactMarkdown from 'react-markdown'
 import CircularProgress from '@mui/material/CircularProgress';
-import { ChevronDown, ChevronUp, BookText, Newspaper, Contact, House, Clock9, Menu, University, BanknoteArrowUp, BanknoteArrowDown, Image as ImageIcon, Music, Book } from 'lucide-react';
+import { ChevronDown, ChevronUp, ShoppingBag, Star, MapPin, ChevronRight, Globe, FileText, Users, ExternalLink, X, BookText, Newspaper, Contact, House, Clock9, Menu, University, BanknoteArrowUp, BanknoteArrowDown, Image as ImageIcon, Music, Book } from 'lucide-react';
 
 const KEYWORD_URLS = [
   { phrase: "Timetable", url: "https://edu.info.uaic.ro/orar/" },
@@ -101,6 +101,12 @@ export default function Home() {
         return true;
     });
 
+  const [audioUrl, setAudioUrl] = useState(null);
+  const mediaRecorderRef = useRef(null);
+  const recordInterval = useRef(null);
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   const resources = [
     { id: 1, name: 'Schedule', icon: Clock9, url: 'https://edu.info.uaic.ro/orar/' },
     { id: 2, name: 'Courses', icon: BookText, url: 'https://edu.info.uaic.ro/' },
@@ -112,9 +118,354 @@ export default function Home() {
     { id: 8, name: 'Housing', icon: House, url: 'https://www.uaic.ro/studenti/cazare/' }
   ];
 
-  const [audioUrl, setAudioUrl] = useState(null);
-  const mediaRecorderRef = useRef(null);
-  const recordInterval = useRef(null);
+  const facultyLinks = [
+  {
+    id: 1,
+    title: "Academic staff",
+    description: "Contact information for professors and staff including office locations, email addresses, and personal websites. Users may search for specific professors by name or department.",
+    url: "https://www.info.uaic.ro/personal-academic",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["professor", "staff", "email", "office", "contact", "website", "person", "faculty", "mr", "mrs", "prof"]
+  },
+  {
+    id: 2,
+    title: "Canteens and cafes",
+    description: "Complete information about campus dining options including: canteen locations, opening hours, menus, and prices. Users may refer to these as 'cafeteria', 'dining hall', or 'food court'.",
+    url: "https://www.uaic.ro/studenti/cantinele-universitatii-alexandru-ioan-cuza/",
+    logo: "/logos/UaicLogo.jpg",
+    keywords: ["canteen", "cafeteria", "food", "menu", "price", "hours", "opening", "eat", "cafe"]
+  },
+  {
+    id: 3,
+    title: "Master programs info",
+    description: "Information about master programs, including websites and study plan link.",
+    url: "https://www.info.uaic.ro/en/master-studies/",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["Software Engineering", "Computational Linguistics", "Computational Optimization", "Artificial Intelligence and Optimization",
+                     "Information Security", "Advanced Studies in Computer Science", "MISS", "ISS", "MLC", "LC", "MIAO", "IAO", "MSD", "SD", "MSI", "SI", "MSAI", "SAI"]
+  },
+  {
+    id: 4,
+    title: "Scholarships",
+    description: "Complete information about scholarship information, including contact persons information, social problems department location, scholarship categories.",
+    url: "https://www.uaic.ro/studenti/burse/",
+    logo: "/logos/UaicLogo.jpg",
+    keywords: ["scholarship", "grant", "financial", "bursary", "support", "tuition", "funding", "merit", "social problems", "social support", "erasmus", "social scholarship"]
+  },
+  {
+    id: 5,
+    title: "Student accommodation",
+    description: "Complete information about student accommodation.",
+    url: "https://www.uaic.ro/studenti/cazare/",
+    logo: "/logos/UaicLogo.jpg",
+    keywords: ["dorm", "room", "bed", "accommodation", "titu", "codrescu", "targusor", "gaudeamus", "akademos", "buna vestire", "live",
+                     "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C10", "C11", "C12", "Titu Maiorescu", "Târguşor-Copou"]
+  },
+  {
+    id: 6,
+    title: "Request for study interruption",
+    description: "Form request for study interruption.",
+    url: "https://profs.info.uaic.ro/~webdata/regulamente/Cerere%20intrerupere%20studii.docx",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["study interruption"]
+  },
+  {
+    id: 7,
+    title: "Request for academic transcript issuance",
+    description: "Form request for academic transcript issuance.",
+    url: "https://profs.info.uaic.ro/~webdata/regulamente/Cerere%20eliberare%20situatie%20scolara.docx",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["academic transcript issuance"]
+  },
+  {
+    id: 8,
+    title: "Request to borrow diploma",
+    description: "Form request to borrow diploma.",
+    url: "https://profs.info.uaic.ro/~webdata/regulamente/Cerere%20imprumut%20diploma.docx",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["borrow diploma"]
+  },
+  {
+    id: 9,
+    title: "Request for faculty withdrawal",
+    description: "Form request for faculty withdrawal.",
+    url: "https://profs.info.uaic.ro/~webdata/regulamente/cerere%20retragere_facultate.doc",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["faculty withdrawal"]
+  },
+  {
+    id: 10,
+    title: "Request for withdrawal submitted to the registry",
+    description: "Form request for withdrawal submitted to the registry.",
+    url: "https://profs.info.uaic.ro/~webdata/regulamente/cerere%20retragere_registratura.doc",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["withdrawal submitted to the registry"]
+  },
+  {
+    id: 11,
+    title: "Medical services",
+    description: "Available medical resources",
+    url: "https://www.uaic.ro/studenti/servicii-medicale/",
+    logo: "/logos/UaicLogo.jpg",
+    keywords: ["medical services"]
+  },
+  {
+    id: 12,
+    title: "Student regulations",
+    description: "Rules of the faculty",
+    url: "https://www.info.uaic.ro/regulamente/",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["student regulations"]
+  },
+  {
+    id: 13,
+    title: "Study plans",
+    description: "Study plans for the students",
+    url: "https://edu.info.uaic.ro/fise-discipline/",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["study plans"]
+  },
+  {
+    id: 14,
+    title: "Schedule bachelor year 1 A",
+    description: "Bachelor's program schedule for Year 1, Semigroup A.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_I1A.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["1A", "first year A", "year 1 A"]
+  },
+  {
+    id: 15,
+    title: "Schedule bachelor year 1 B",
+    description: "Bachelor's program schedule for Year 1, Semigroup B.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_I1B.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["1B", "first year B", "year 1 B"]
+  },
+  {
+    id: 16,
+    title: "Schedule bachelor year 1 E",
+    description: "Bachelor's program schedule for Year 1, Semigroup E.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_I1E.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["1E", "first year E", "year 1 E"]
+  },
+  {
+    id: 17,
+    title: "Schedule bachelor year 2 A",
+    description: "Bachelor's program schedule for Year 2, Semigroup A.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_I2A.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["2A", "second year A", "year 2 A"]
+  },
+  {
+    id: 18,
+    title: "Schedule bachelor year 2 B",
+    description: "Bachelor's program schedule for Year 2, Semigroup B.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_I2B.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["2B", "second year B", "year 2 B"]
+  },
+  {
+    id: 19, 
+    title: "Schedule bachelor year 2 E",
+    description: "Bachelor's program schedule for Year 2, Semigroup E.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_I2E.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["2E", "second year E", "year 2 E"]
+  },
+  {
+    id: 20,
+    title: "Schedule bachelor year 3 A",
+    description: "Bachelor's program schedule for Year 3, Semigroup A.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_I3A.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["3A", "third year A", "year 3 A"]
+  },
+  {
+    id: 21,
+    title: "Schedule bachelor year 3 B",
+    description: "Bachelor's program schedule for Year 3, Semigroup B.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_I3B.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["3B", "third year B", "year 3 B", "error"]
+  },
+  {
+    id: 22,
+    title: "Schedule bachelor year 3 E",
+    description: "Bachelor's program schedule for Year 3, Semigroup E.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_I3E.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["3E", "third year E", "year 3 E"]
+  },
+  {
+    id: 23,
+    title: "Schedule master IAO year 1",
+    description: "Artificial Intelligence and Optimization master's program schedule for Year 1.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MIAO1.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MIAO1", "IAO1"]
+  },
+  {
+    id: 24,
+    title: "Schedule master IAO year 2",
+    description: "Artificial Intelligence and Optimization master's program schedule for Year 2.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MIAO2.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MIAO2", "IAO2"]
+  },
+  {
+    id: 25,
+    title: "Schedule master LC year 1",
+    description: "Computational Linguistics master's program schedule for Year 1.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MLC1.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MLC1", "LC1"]
+  },
+  {
+    id: 26,
+    title: "Schedule master LC year 2",
+    description: "Computational Linguistics master's program schedule for Year 2.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MLC2.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MLC2", "LC2"]
+  },
+  {
+    id: 27,
+    title: "Schedule master SI year 1",
+    description: "Information Security master's program schedule for Year 1.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MSI1.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MSI1", "SI1"]
+  },
+  {
+    id: 28,
+    title: "Schedule master SI year 2",
+    description: "Information Security master's program schedule for Year 2.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MSI2.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MSI2", "SI2"]
+  },
+  {
+    id: 29,
+    title: "Schedule master SAI year 1",
+    description: "Advanced Studies in Computer Science master's program schedule for Year 1.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MSAI1.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MSAI1", "SAI1"]
+  },
+  {
+    id: 30,
+    title: "Schedule master SAI year 2",
+    description: "Advanced Studies in Computer Science master's program schedule for Year 2.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MSAI2.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MSAI2", "SAI2"]
+  },
+  {
+    id: 31,
+    title: "Schedule master SD year 1",
+    description: "Distributed Systems master's program schedule for Year 1.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MSD1.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MSD1", "SD1"]
+  },
+  {
+    id: 32,
+    title: "Schedule master SD year 2",
+    description: "Distributed Systems master's program schedule for Year 2.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MSD2.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MSD2", "SD2"]
+  },
+  {
+    id: 33,
+    title: "Schedule master ISS1 year 1",
+    description: "Software Engineering master's program schedule for Year 1 Group 1.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MISS11.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MISS11", "ISS11"]
+  },
+  {
+    id: 34,
+    title: "Schedule master ISS2 year 1",
+    description: "Software Engineering master's program schedule for Year 1 Group 2.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MISS12.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MISS12", "ISS12"]
+  },
+  {
+    id: 35,
+    title: "Schedule master ISS1 year 2",
+    description: "Software Engineering master's program schedule for Year 2 Group 1.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MISS21.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MISS21", "ISS21"]
+  },
+  {
+    id: 36,
+    title: "Schedule master ISS2 year 2",
+    description: "Software Engineering master's program schedule for Year 2 Group 2.",
+    url: "https://edu.info.uaic.ro/orar/participanti/orar_MISS22.html",
+    logo: "/logos/FiiLogo.png",
+    keywords: ["MISS22", "ISS22"]
+  }
+];
+
+  const filteredLinks = (message) => {
+    const messageLower = message.toLowerCase();
+    return facultyLinks.filter(link => {
+      return link.keywords?.some(keyword => 
+        messageLower.includes(keyword.toLowerCase())
+      )
+    })
+  };
+
+  const facultySidebar = ({ isOpen, onClose, links }) => {
+    if (!isOpen) return null;
+
+    return (
+      <>
+        <div 
+          className={styles.sidebarOverlay}
+          onClick={onClose}
+        />
+        
+        <div className={styles.sidebar}>
+          <div className={styles.sidebarHeader}>
+            <h3>Faculty links</h3>
+            <button 
+              className={styles.closeButton}
+              onClick={onClose}
+            >
+              ✕
+            </button>
+          </div>
+          
+          <div className={styles.sidebarContent}>
+            {links.map((link) => (
+              <div key={link.id} className={styles.facultyResource}>
+                <div className={styles.facultyResourceHeader}>
+                  <img src={link.logo} className={styles.facultyLogo} />
+                </div>
+                
+                <h4 className={styles.facultyResourceTitle}>{link.title}</h4>
+                <p className={styles.facultyResourceDescription}>{link.description}</p>
+                
+                <a 
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.facultyButton}
+                  onClick={onClose}
+                >
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  };
 
   useEffect(() => {
     function handleClickOutside(event) {
@@ -207,6 +558,14 @@ export default function Home() {
         setMessages((prevMessages) => [...prevMessages, { "message": data.answer, "type": "apiMessage" }]);
         setUserInput("")
         setLoading(false);
+
+         const links = extractRelevantLinks(data.answer);
+         setRelevantLinks(links);
+          
+         // Auto-open sidebar if there are relevant links
+         if (links.length > 0) {
+           setSidebarOpen(true);
+         }
     };
 
     const handleQuickQuestionClick = async (question) => {
@@ -279,6 +638,10 @@ export default function Home() {
         if(resource.url) {
           window.open(resource.url, '_blank', 'noopener,noreferrer');
         }
+    };
+
+    const handleSidebarResourceClick = (url) => {
+        window.open(url, '_blank', 'noopener,noreferrer');
     };
 
     useEffect(() => {
@@ -508,10 +871,11 @@ export default function Home() {
                   </div>
                 </div>
               ) : (
-                messages.map((message, index) => (
-                  <div
-                    key={index}
-                    className={message.type === "apiMessage" ? styles.botText : styles.userBubble}
+                messages.reduce((acc, message, index) => {
+                  acc.push(
+                  <div 
+                     key={index}
+                     className={message.type === "apiMessage" ? styles.botText : styles.userBubble}
                   >
                     <div className={styles.markdownanswer}>
                       {message.isAudio ? (
@@ -528,12 +892,29 @@ export default function Home() {
                           {processKeywords(message.message)}
                         </ReactMarkdown>
                       )}
+                      </div>
                     </div>
-                  </div>
-                ))
-              )}
+                  );
+                        if(message.type === "apiMessage") {
+                        acc.push(
+                        <div key={`faculty-${index}`} className={styles.facultyResourceContainer}>
+                          <button
+                            onClick={() => setIsSidebarOpen(true)}
+                            className={styles.facultyResourceButton}
+                          >
+                            Resources
+                          </button>
+                        </div>
+                      );
+                    }
+
+                    return acc;
+                 }, []) 
+                )
+              }
             </div>
-          </div>     
+          </div>
+          
           {messages.length > 0 && (
           <div className={styles.center}>
             <div className={styles.cloudform}>
@@ -592,7 +973,12 @@ export default function Home() {
           </div>  
           )}                     
         </main>
+          <facultySidebar 
+            isOpen={isSidebarOpen}
+            onClose={() => setIsSidebarOpen(false)}
+            links={filteredLinks || []}
+          />
       </div>
     </>
-    );
+  );
 }
